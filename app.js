cecc112a485a01e0cc3880341ad13f959b5c55bd
prihdiz <82004556+prihdiz@users.noreply.github.com>
2021-04-06 01:08:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
-app.get('/', (req, res) => {
-  res.send('Hello 23BPM - A melhor turma')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
 git add app.js
 git commit -m "Full pipeline test"
-git push
