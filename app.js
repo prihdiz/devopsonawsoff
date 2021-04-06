df0b6de3db6021a6601480247a479ed367f4c91a
prihdiz <82004556+prihdiz@users.noreply.github.com>
2021-04-06 00:55:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello RM338674 - FIAP23BPM')
+  res.send('Hello 23BPM - A melhor turma')
 })
 
 app.listen(port, () => {
